eac2f569c4b51a32d37193081cc07a0c11fffd85
N-Sashank <sashank407@gmail.com>
2024-11-21 18:08:09 +0530

    add get delivery persons route

--- src/app/api/delivery_persons/route.ts
+++ src/app/api/delivery_persons/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/app/db";
-import { delivery_personsTable } from "@/app/db/schema";
+import { delivery_personsTable, warehousesTable } from "@/app/db/schema";
 import { deliverypersonSchema } from "@/validator/deliverypersonSchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request:Request) {
     const data=await request.json()
@@ -21,4 +22,18 @@ export async function POST(request:Request) {
         return Response.json({message:"data not inserted to db",error},{status:500});
     }
 
+}
+
+export async function GET(){
+    try {
+        const list=await db.select({
+            id:delivery_personsTable.id,
+            name:delivery_personsTable.name,
+            phone:delivery_personsTable.phone,
+            warehouse:warehousesTable.name,
+        }).from(delivery_personsTable).leftJoin(warehousesTable,eq(warehousesTable.id,delivery_personsTable.warehouse_id)).orderBy(desc(delivery_personsTable.id));
+        return Response.json(list,{status:201})
+    } catch (error) {
+        return Response.json({message:"db call failed"},{status:500})
+    }
 }

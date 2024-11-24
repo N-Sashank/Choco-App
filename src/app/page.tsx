04b039948fd1c4e274e0af33d180378b930fb217
N-Sashank <sashank407@gmail.com>
2024-11-24 07:14:03 +0530

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+ 
 
 export default function Home() {
   return (
    <>
    <h1>choco-app</h1>
+   <Button >click me </Button>
    </>
   );
 }
